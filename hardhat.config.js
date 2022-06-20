5a31d0ede816f058a870eca0571f6b81ddc9041b
Jocelyn <82773188+ragueneau@users.noreply.github.com>
2022-06-19 22:25:46 -0400

    Hardhat  config

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,8 @@ module.exports = {
       url: "http://127.0.0.1:8545"
     },
     coeptix: {
-      url: "https://ethernode.coeptix.net"
+      url: "https://ethernode.coeptix.net",
+      accounts: [ "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d"]
     },
     hardhat: {
       // See its defaults
